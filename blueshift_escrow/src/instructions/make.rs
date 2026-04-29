979e7c58158272ce899cbda36559b7963db05547
Abhineet Biju <abhi@abhineetbiju.com>
2026-04-29 23:10:48 +0530

    feat: validate make instruction accounts
    - verify maker signature and mint initialization
    
    - validate maker token account mint and owner before escrow creation

--- blueshift_escrow/src/instructions/make.rs
+++ blueshift_escrow/src/instructions/make.rs
@@ -1,4 +1,5 @@
 use pinocchio::{error::ProgramError, AccountView};
+use pinocchio_token::state::{Account as TokenAccount, Mint};
 
 pub struct MakeAccounts<'a> {
     pub maker: &'a AccountView,
@@ -20,5 +21,42 @@ impl<'a> TryFrom<&'a [AccountView]> for MakeAccounts<'a> {
         else {
             return Err(ProgramError::NotEnoughAccountKeys);
         };
+
+        //Basic checks
+        if !maker.is_signer() {
+            return Err(ProgramError::MissingRequiredSignature);
+        }
+
+        let mint_a_data = Mint::from_account_view(mint_a)?;
+        let mint_b_data = Mint::from_account_view(mint_b)?;
+
+        if !mint_a_data.is_initialized() || !mint_b_data.is_initialized() {
+            return Err(ProgramError::InvalidAccountData);
+        }
+
+        let maker_ata_a_data = TokenAccount::from_account_view(maker_ata_a)?;
+
+        if !maker_ata_a_data.is_initialized() {
+            return Err(ProgramError::InvalidAccountOwner);
+        }
+
+        if maker_ata_a_data.mint() != mint_a.address() {
+            return Err(ProgramError::InvalidAccountData);
+        }
+
+        if maker_ata_a_data.owner() != maker.address() {
+            return Err(ProgramError::InvalidAccountData);
+        }
+
+        Ok(Self {
+            maker,
+            escrow,
+            mint_a,
+            mint_b,
+            maker_ata_a,
+            vault,
+            system_program,
+            token_program,
+        })
     }
 }
