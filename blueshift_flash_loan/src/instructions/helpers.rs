9a49e4b7ed176aa0a39de32ae3e241f812fa71d5
Abhineet Biju <abhi@abhineetbiju.com>
2026-05-13 16:03:41 +0530

    feat: implement helpers.rs in blueshift_flash_loan

---
+++ blueshift_flash_loan/src/instructions/helpers.rs
@@ -0,0 +1,23 @@
+use pinocchio::{error::ProgramError, AccountView};
+
+#[repr(C, packed)]
+pub struct LoanData {
+    pub protocol_token_account: [u8; 32],
+    pub balance: u64,
+}
+
+pub fn get_token_amount(data: &[u8], account: &AccountView) -> Result<u64, ProgramError> {
+    if !account.owned_by(&pinocchio_token::ID) {
+        return Err(ProgramError::InvalidAccountOwner);
+    }
+
+    if account.data_len().ne(&pinocchio_token::state::Account::LEN) {
+        return Err(ProgramError::InvalidAccountData);
+    }
+
+    if data.len() != pinocchio_token::state::Account::LEN {
+        return Err(ProgramError::InvalidAccountData);
+    }
+
+    Ok(u64::from_le_bytes(data[64..72].try_into().unwrap()))
+}
