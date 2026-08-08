e19116fc929b4ba10bed40ebab0dae87de15ad1c
Abhineet Biju <abhi@abhineetbiju.com>
2026-08-08 20:05:22 +0530

    chore(amm): wire up instructions scaffold

--- blueshift_native_amm/src/lib.rs
+++ blueshift_native_amm/src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use pinocchio::{
+    AccountView, Address, ProgramResult, entrypoint, error::ProgramError, nostd_panic_handler,
+};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+entrypoint!(process_instruction);
+pub mod instructions;
+pub mod state;
+pub use instructions::*;
+pub use state::*;
